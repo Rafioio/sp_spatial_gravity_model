60b1b954116ca1124eb9c7732f2fdf625e26fade
rafael <rafaelcampellosoares@gmail.com>
2026-07-30 20:51:50 -0300

    Refactor cost collection module import

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from src.utils.centroide_ponderado import main as centroide_ponderado
 from src.construcao_matriz_demanda import main as construcao_matriz_demanda
 from src.utils.area_urbana import main as area_urbana
-from src.custo_de_coleta import main as custo_de_coleta
+from src.construcao_custo_de_coleta import main as construcao_custo_de_coleta
 
 
 def main():
     centroide_ponderado()
     construcao_matriz_demanda()
     area_urbana()
-    custo_de_coleta()
+    construcao_custo_de_coleta()
     print("Fluxo concluído com sucesso!")
 
 
