2ecea3e3be836fdf25007214028031893c8b4b1e
rafael <rafaelcampellosoares@gmail.com>
2026-07-30 12:33:05 -0300

    Implement distance calculation utility

---
+++ src/utils/calcular_distancia.py
@@ -0,0 +1,11 @@
+from math import atan2, cos, radians, sin, sqrt
+
+def calcular_distancia(lat1, lon1, lat2, lon2):
+    lat1_rad, lon1_rad = radians(lat1), radians(lon1)
+    lat2_rad, lon2_rad = radians(lat2), radians(lon2)
+    dlon = lon2_rad - lon1_rad
+    dlat = lat2_rad - lat1_rad
+    a = sin(dlat / 2)**2 + cos(lat1_rad) * cos(lat2_rad) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return 6371.0 * c
+
