8742e5ae299f6bce613baa078ec365dee74bf24a
rafael <rafaelcampellosoares@gmail.com>
2026-07-30 12:33:05 -0300

    Refactor function imports in main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from src.utils.centroide_ponderado import main as centroid_main
-from src.construcao_matriz_demanda import main as gravity_main
-from src.utils.area_urbana import main as ca_main
-from src.custo_de_coleta import main as custo_main
+from src.utils.centroide_ponderado import main as centroide_ponderado
+from src.construcao_matriz_demanda import main as construcao_matriz_demanda
+from src.utils.area_urbana import main as area_urbana
+from src.custo_de_coleta import main as custo_de_coleta
 
 
 def main():
-    centroid_main()
-    gravity_main()
-    ca_main()
-    custo_main()
+    centroide_ponderado()
+    construcao_matriz_demanda()
+    area_urbana()
+    custo_de_coleta()
     print("Fluxo concluído com sucesso!")
 
 
