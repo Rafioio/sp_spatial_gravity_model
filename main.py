8686c6f13b692a7a5f70901b38b3134b1652df2b
rafael <rafaelcampellosoares@gmail.com>
2026-07-30 21:39:42 -0300

    Refactors main import and adds instance generator

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from src.utils.centroide_ponderado import main as centroide_ponderado
 from src.construcao_matriz_demanda import main as construcao_matriz_demanda
 from src.utils.area_urbana import main as area_urbana
-from src.construcao_custo_de_coleta import main as construcao_custo_de_coleta
-
+from src.construcao_matriz_custo_de_coleta import main as construcao_custo_de_coleta
+from src.utils.gerador_instancia import main as gerar_instancia
 
 def main():
     centroide_ponderado()
     construcao_matriz_demanda()
     area_urbana()
     construcao_custo_de_coleta()
+    gerar_instancia()
     print("Fluxo concluído com sucesso!")
 
 
